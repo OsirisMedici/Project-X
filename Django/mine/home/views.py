71ef270b8c228e0abe005deab4b69993155fd13b
Abhishek <abhishekphd14@gmail.com>
2022-02-20 22:05:46 +0530

    Update views.py

--- Django/mine/home/views.py
+++ Django/mine/home/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render, HttpResponse
 def index (request):
     return HttpResponse("This is the Home page")
 
+def about (request):
+    return HttpResponse("This is the about page")
+
+def contact (request):
+    return HttpResponse("This is the contact page")
+
+def help (request):
+    return HttpResponse("This is the help page")
+
 # Create your views here.
