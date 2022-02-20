0e91492f3d8e55c2215232495506f4a08af1199b
Abhishek <abhishekphd14@gmail.com>
2022-02-20 21:46:33 +0530

    yk

--- Django/mysite/polls/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def index (request):
-    return HttpResponse('hello World, We are here.. and you are at the Polls Index')
-
-def index (contact):
-    return HttpResponse('hello World, We are here.. and you are at the Polls Index')
