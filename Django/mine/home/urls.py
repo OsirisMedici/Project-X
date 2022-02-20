7571b2844ce6558f93e9877c0ca95fe7835b54ef
Abhishek <abhishekphd14@gmail.com>
2022-02-20 22:05:38 +0530

    Update urls.py

--- Django/mine/home/urls.py
+++ Django/mine/home/urls.py
@@ -1,6 +1,11 @@
-from django.contrib import admin
-from django.urls import path
+
+from django.urls import path, include
+from home import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path ("", views.index, name='home'),
+    path ('about', views.about, name='about' ),
+    path ('contact', views.about, name='contact' ),
+    path ('help', views.about, name='help' )
+    
 ]
