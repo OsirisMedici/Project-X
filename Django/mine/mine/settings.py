63243940aaa023e0507db85d6c734abd5b6d1e55
Abhishek <abhishekphd14@gmail.com>
2022-02-21 23:17:10 +0530

    Update settings.py

--- Django/mine/mine/settings.py
+++ Django/mine/mine/settings.py
@@ -123,3 +123,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Added Manually here 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
