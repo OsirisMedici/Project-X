424bc21273eee1872f2f35020d94684ac4a63e5f
Abhishek <abhishekphd14@gmail.com>
2022-02-21 23:16:57 +0530

    Update urls.py

--- Django/mine/home/urls.py
+++ Django/mine/home/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from home import views
 
 urlpatterns = [
-    path ("", views.index, name='home'),
+    path ("", views.home, name='home'),
     path ('about', views.about, name='about' ),
     path ('contact', views.about, name='contact' ),
     path ('help', views.about, name='help' )
