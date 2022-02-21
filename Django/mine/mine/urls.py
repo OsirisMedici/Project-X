abc73d20e7af994027c301a3c5a5c6c97d47840a
Abhishek <abhishekphd14@gmail.com>
2022-02-22 00:43:07 +0530

    Update urls.py

--- Django/mine/mine/urls.py
+++ Django/mine/mine/urls.py
@@ -18,6 +18,10 @@ from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Abhishekvrt Admin"
+admin.site.site_title = "Abhishek Admin Portal"
+admin.site.index_title = "Welcome to App Portal"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path ('', include('home.urls'), name=home)
